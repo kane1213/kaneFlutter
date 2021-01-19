2d8204197bf04329240ec0d1977b7727f2931af4
FlutLab.io <>
2021-01-19 08:11:55 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,11 @@ import 'package:flutter/material.dart';
 // import 'package:provider_setup.dart';
 // import 'package:provider/provider.dart';
 
-void main() => runApp(MultiProvider(
-    //   providers: providers,
-      child: MyApp(),
-    ));
+// void main() => runApp(MultiProvider(
+//     //   providers: providers,
+//       child: MyApp(),
+//     ));
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
