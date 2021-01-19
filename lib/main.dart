5e5c1877b3ece601bb8d6bea1f847e1b3ddfe807
FlutLab.io <>
2021-01-19 08:27:42 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -1,42 +1,18 @@
-import 'package:flutter/material.dart';
-import 'views/HomePage.dart';
-// import 'package:provider_setup.dart';
-// import 'package:provider/provider.dart';
-
-// void main() => runApp(MultiProvider(
-//     //   providers: providers,
-//       child: MyApp(),
-//     ));
-void main() => runApp(MyApp());
-
-class MyApp extends StatelessWidget {
+class HomePage extends StatelessWidget {
+  final package:kane:/String title;
+  const HomePage({@required this.title});
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        title: 'Motobike Accessary',
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
+    return Scaffold(
+      appBar: AppBar(
+        // The title text which will be shown on the action bar
+        title: Text(title),
+      ),
+      body: Center(
+        child: Text(
+          'Kane Hello, World!',
         ),
-        home: HomePage(title: "KANE")
+      ),
     );
   }
 }
-
-// class HomePage extends StatelessWidget {
-//   final String title;
-//   const HomePage({@required this.title});
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         // The title text which will be shown on the action bar
-//         title: Text(title),
-//       ),
-//       body: Center(
-//         child: Text(
-//           'Kane Hello, World!',
-//         ),
-//       ),
-//     );
-//   }
-// }
