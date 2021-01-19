afb580379a1dbb1bedc6a0cbcdec74e6334396e0
FlutLab.io <>
2021-01-19 08:17:04 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import './views/HomePage.dart';
+// import './views/HomePage.dart';
 // import 'package:provider_setup.dart';
 // import 'package:provider/provider.dart';
 
@@ -18,6 +18,25 @@ class MyApp extends StatelessWidget {
           primarySwatch: Colors.blue,
         ),
         home: HomePage("KANE")
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  final String title;
+  const HomePage({@required this.title});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        // The title text which will be shown on the action bar
+        title: Text(title),
+      ),
+      body: Center(
+        child: Text(
+          'Kane Hello, World!',
+        ),
+      ),
     );
   }
 }
