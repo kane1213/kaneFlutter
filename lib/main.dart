323f0dbc5429d627b5372f55370dff89c271a3e1
FlutLab.io <>
2021-01-19 08:31:04 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,30 @@
-class HomePage extends StatelessWidget {
-  final package:kane:/String title;
-  const HomePage({@required this.title});
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      // Application name
+      title: 'Kane Hello World',
+      // Application theme data, you can set the colors for the application as
+      // you want
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      // A widget which will be started on application startup
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  final String title;
+
+  const MyHomePage({@required this.title});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
