7e9be13eecd847b8067716df1d1c7af7626aef6e
FlutLab.io <>
2021-01-19 08:20:12 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-// import './views/HomePage.dart';
+import './views/HomePage.dart';
 // import 'package:provider_setup.dart';
 // import 'package:provider/provider.dart';
 
@@ -22,21 +22,21 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class HomePage extends StatelessWidget {
-  final String title;
-  const HomePage({@required this.title});
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text(
-          'Kane Hello, World!',
-        ),
-      ),
-    );
-  }
-}
+// class HomePage extends StatelessWidget {
+//   final String title;
+//   const HomePage({@required this.title});
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         // The title text which will be shown on the action bar
+//         title: Text(title),
+//       ),
+//       body: Center(
+//         child: Text(
+//           'Kane Hello, World!',
+//         ),
+//       ),
+//     );
+//   }
+// }
